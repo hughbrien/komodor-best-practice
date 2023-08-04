ff04c8f425fe23d445ba4f1a3035056e9d8c1aec
hughbrien <hugh@hughbrien.com>
2023-08-04 14:11:43 -0400

    test updates

--- create-best-practice.sh
+++ create-best-practice.sh
@@ -1,2 +1,3 @@
 kubectl create ns improve-service
 kubectl apply -f  shipping.yaml -n improve-service
+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
--- improve-service.sh
+++ improve-service.sh
@@ -1,1 +1,2 @@
+# Notes
 kubectl apply -f  shipping-step-2.yaml -n improve-service
